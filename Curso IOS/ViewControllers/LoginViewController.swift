9b8d018f26215e9ff45c20f019f1dafa2f2da553
Matias Mansilla <dmatiasmansilla@gmail.com>
2020-04-26 02:44:24 -0300

    Funcionando el show del LoginViewController desde el main

---
+++ Curso IOS/ViewControllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Curso IOS
+//
+//  Created by Dardo Matias Mansilla on 25/04/2020.
+//  Copyright © 2020 Dardo Matias Mansilla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Curso IOS/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Curso IOS
+//
+//  Created by Dardo Matias Mansilla on 25/04/2020.
+//  Copyright © 2020 Dardo Matias Mansilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func logintaped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "LoginViewController", bundle: nil)
+        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        navigationController?.pushViewController(loginVC, animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
